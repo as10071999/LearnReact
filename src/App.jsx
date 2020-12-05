97c37a244ff63888c4d63b6fed7c220a2ba81256
Aryan Singh <as10071999@gmail.com>
2020-12-06 00:34:37 +0530

    "Latest code"

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,30 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [listItem, setListItem] = useState([]);
+  function itemEvent(event) {
+    console.log(event.target.value);
+    setItem(event.target.value);
+  }
+  function showList() {
+    console.log(listItem);
+    setListItem([...listItem, item]);
+  }
   return (
     <>
-      <h1>To Do List</h1>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>To Do List</h1>
+          <br />
+          <input type="text" placeholder="Add a item" onChange={itemEvent} />
+          <button onClick={showList}>+</button>
+          <ol>
+            <li>buy {listItem}</li>
+          </ol>
+        </div>
+      </div>
     </>
   );
 }
