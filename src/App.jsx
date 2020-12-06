4c18594f599279aaf99ab4651ec935dcd2615509
Aryan Singh <as10071999@gmail.com>
2020-12-06 11:09:37 +0530

    implemented Increase and Decrease counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+  function IncreaseCount() {
+    setNum((prev) => {
+      return prev + 1;
+    });
+  }
+  function DecreaseCount() {
+    setNum((prev) => {
+      return prev !== 0 ? prev - 1 : 0;
+    });
+  }
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{num}</h1>
+          <div className="btn_div">
+            <button onClick={IncreaseCount}>Increment</button>
+            <button onClick={DecreaseCount}>Decrement</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
